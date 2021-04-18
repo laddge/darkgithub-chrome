31b30ce8cecc71d8daf05182766857845488f9b2
Laddge <dev.laddge@gmail.com>
2021-04-18 18:35:12 +0900

    resolve error

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 (function () {
-    html = document.getElementsByTagName('html');
+    html = document.getElementsByTagName('html')[0];
     html.setAttribute('data-color-mode', 'dark');
     html.setAttribute('data-light-theme', 'light');
     html.setAttribute('data-dark-theme', 'dark');
