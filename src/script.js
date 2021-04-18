b5c24522a7a99aeae6048ae1a4d655513cd4851c
Laddge <dev.laddge@gmail.com>
2021-04-18 18:18:27 +0900

    add src

---
+++ src/script.js
@@ -0,0 +1,6 @@
+(function () {
+    html = document.getElementsByTagName('html');
+    html.setAttribute('data-color-mode', 'dark');
+    html.setAttribute('data-light-theme', 'light');
+    html.setAttribute('data-dark-theme', 'dark');
+})();
